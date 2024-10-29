cbcf85c1ef554a2e48fea10b0354d3453d170d23
Shashwamie <shashwamie@gmail.com>
2024-10-29 00:25:08 -0600

    trying this even though it says error

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './login.jsx'
+import Login from './Login.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
