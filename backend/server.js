2f24f391dafb0aceaa4abc8d4982c3395cc9675d
Bryce Barbee <shashwamie@gmail.com>
2025-02-23 00:57:21 -0700

    Added env for server string and crated connectDB function to connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Server is ready ');
-});
+dotenv.config();
+app.use(express.json()); // to accept JSON data in the body
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
+  connectDB()
   console.log('Server is running on http://localhost:3000');
 });
+
+
