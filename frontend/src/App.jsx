fce017c3a3cb2eb3d4923225790d094b20e9f5de
Bryce Barbee <shashwamie@gmail.com>
2025-02-24 05:32:36 -0700

    Added onboard to use for displayVantage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import { Box } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import HomePage from './pages/HomePage.jsx'
+import Onboard from './Pages/Onboard.jsx'
 import { useColorModeValue } from './components/ui/color-mode.jsx'
 
 function App() {
   return (
     <>
       <Box bg={useColorModeValue('gray.300', 'gray.900')} minH={"100vh"}>
-        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path='/Onboard' element={<Onboard />} />
         </Routes>
       </Box>
     </>
---
+++ frontend/src/Pages/Onboard.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Container, Flex, Text, Button, Box, Grid, GridItem, Image, Float, HStack, VStack } from '@chakra-ui/react'
+
+const Onboard = () => {
+  return (
+    <Box bg={"gray.100"} style={{height: "100vh"}} p={6}>
+        <Flex w="full" justifyContent={"space-between"} alignItems={"center"} flexDir={{xl: "row", mdDown: "column"}}>
+            <Flex align={"center"} gap={"6"} flexDir={{xl: "row", mdDown: "column"}}>
+                <Image src="./src/assets/outlined-placeholder-blank.svg" h={20}/>
+                <Text fontFamily={"Esteban"} fontSize={40}  color={"black"}>
+                    Display Vantage
+                </Text>
+            </Flex>
+            <Flex  gap="4">
+                <Button size={24} p={2} bg={"gray.100"}>
+                    Learn More
+                </Button>
+                <Button size={24} p={2} bg={"gray.100"} style={{borderColor: "#B1BFFF", color: "#B1BFFF"}} borderWidth={3}>
+                    Explore Options
+                </Button>
+                <Button size={24} p={2} style={{backgroundColor: "#011936", color: "white"}}>
+                    Login
+                </Button>
+            </Flex>
+        </Flex>
+        <Flex paddingLeft={20} paddingRight={10} paddingTop={{xl: "12", lg: "10", smDown: "8"}} gap={{xl: "16", lg: "12", smDown: "8"}} 
+         align={"center"} justifyContent={"space-around"} flexDir={{xl: "row", mdDown: "column"}}>
+            <VStack gap={{xl: "44", lg: "28", smDown: "12"}}>
+                <Box>
+                    <Text textStyle={{xl: "6xl", lg: "4xl", smDown: "3xl"}} color={"black"} fontWeight={"bold"} maxW={"42rem"}>
+                        Digital Signage made easy and accessible for{"\n"}
+                    <Text as={"span"} style={{color: "#ED254E"}}>
+                        Everyone
+                    </Text>
+                    </Text>
+                </Box>
+                <Text textStyle={{xl: "4xl", lg: "2xl", smDown: "xl"}} style={{color: "#465362"}} maxW={"42rem"} >
+                    Create and display digital signs to all your businesses screens with an easy to use web appliction to edit, manage and display your content.
+                </Text>
+            </VStack>
+            <Box >
+                <Image src="./src/assets/DVImage.svg" htmlWidth={"1000rem"} />
+            </Box>
+        </Flex>
+        <Flex justifyContent={"end"} paddingRight={{xl: "52", lg: "32", smDown: "12"}} paddingTop={{xl: "24", lg: "12", smDown: "6"}}>
+            <Button textStyle={{xl: "5xl", lg: "2xl", sm: "xl"}} style={{ backgroundColor: "#f9DC5C"}} p={{xl: "10", lg: "8", smDown: "4"}} rounded={"xl"} marginBottom={{xl: "10", lg: "8", smDown: "4"}}>
+                Register This Screen
+            </Button>
+        </Flex>
+    </Box>
+  )
+}
+
+export default Onboard
